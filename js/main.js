e12cddf746ee1ef7090e2bf625e3e43ec6f7b5c1
vanloc <vanloc18021998@gmail.com>
2020-03-17 04:46:57 +0700

    add topbar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var type = new Typed(".typed", {
-  strings: ["Welcome to my site!", "Thanks for visit"],
+  strings: ["A student with a passion for AI / ML.", "Let me show you what I can do ❤️"],
   stringsElement: null,
   // typing speed
-  typeSpeed: 30,
+  typeSpeed: 50,
   // time before typing starts
   startDelay: 500,
   // backspacing speed
-  backSpeed: 20,
+  backSpeed: 30,
   // time before backspacing
-  backDelay: 500,
+  backDelay: 1000,
   // loop
   loop: true,
   // false = infinite
@@ -29,4 +29,38 @@ var type = new Typed(".typed", {
   // onStringTyped: function() {},
   // // callback for reset
   // resetCallback: function() {}
+});
+
+$("#wrapper").click(function() {
+  $(".icon").toggleClass("close");
+  var x = document.getElementById("menu");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+$(window).resize(function() {
+  var w = $(window).width();
+  var element = document.getElementById("name-effect");
+  var x = document.getElementById("menu");
+  if (w <= 760) {
+    element.className = element.className.replace(/\glitch-effect\b/g, "");
+    x.style.display = "none";
+  } else if (!element.classList.contains("glitch-effect")) {
+    element.classList.add("glitch-effect");
+    x.style.display = "inline-flex";
+  }
+});
+
+$(document).ready(function() {
+  let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+  if (isMobile) {
+    var element = document.getElementById("name-effect");
+    element.className = element.className.replace(/\glitch-effect\b/g, "");
+    var x = document.getElementById("menu");
+    x.style.display = "none";
+  }
 });
